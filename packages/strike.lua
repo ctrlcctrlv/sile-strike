d64f9ac372f3de160e60610f9bc8d8ad3cdf85ae
Caleb Maclennan <caleb@alerque.com>
2022-08-23 13:59:31 +0300

    Update module to SILE v0.14 compatible, add rockspec (#4)
    * Add rockspec
    
    * Refactor with SILE v0.14+ module API
    
    * Change zw to pt unit, seems to be original intent

--- packages/strike.lua
+++ packages/strike.lua
@@ -1,42 +1,56 @@
-SILE.baseClass:loadPackage("rules")
-SILE.baseClass:loadPackage("rebox")
-SILE.baseClass:loadPackage("raiselower")
-SILE.baseClass:loadPackage("color")
+local base = require("packages.base")
 
-SILE.registerCommand("strike", function(options, content)
+local package = pl.class(base)
+package._name = "strike"
+
+function package:_init ()
+  base._init(self)
+  self.class:loadPackage("rules")
+  self.class:loadPackage("rebox")
+  self.class:loadPackage("raiselower")
+  self.class:loadPackage("color")
+end
+
+function package:registerCommands ()
+
+  self:registerCommand("strike", function(options, content)
     -- next 2 lines from float in frametricks.lua
     local hbox = SILE.call("hbox", {}, content)
     table.remove(SILE.typesetter.state.nodes) -- steal it back
 
-    SILE.call("rebox", {width = 0}, content)
-    local first = options.raise or SILE.parseComplexFrameDimension("2zw")
+    SILE.call("rebox", { width = 0 }, content)
+    local first = options.raise or SILE.measurement("2pt")
     local i = 0
-    local height = options.height or SILE.parseComplexFrameDimension("1.5zw")
+    local height = options.height or SILE.measurement("1.5pt")
 
     if options.n and options.n == "triple" then
-        i = 2
-        first = options.raise or SILE.parseComplexFrameDimension("1.5zw")
+      i = 2
+      first = options.raise or SILE.measurement("1.5pt")
     elseif options.n and options.n == "double" then
-        i = 1
+      i = 1
     end
 
     if i > 0 and not options.height then
-        height = SILE.parseComplexFrameDimension("1.0zw")
+      height = SILE.measurement("1pt")
     end
 
     if options.color then
-        SILE.call("color", {color = options.color})
+      SILE.call("color", { color = options.color })
     end
 
     for _ = 0, i, 1 do
-        SILE.call("rebox", {width = 0}, function()
-            SILE.call("raise", {height = first}, function()
-                SILE.call("color", {color = options.color or "black"}, function()
-                    SILE.call("hrule", {width = hbox.width, height = height})
-                end)
-            end)
+      SILE.call("rebox", { width = 0 }, function()
+        SILE.call("raise", { height = first }, function()
+          SILE.call("color", { color = options.color or "black" }, function()
+            SILE.call("hrule", { width = hbox.width, height = height })
+          end)
         end)
-        first = first + SILE.parseComplexFrameDimension("2zw")
+      end)
+      first = first + SILE.measurement("2pt")
     end
-    SILE.call("glue", {width = hbox.width})
-end, "Strikethrough text")
+    SILE.call("glue", { width = hbox.width })
+  end, "Strikethrough text")
+
+end
+
+return package
